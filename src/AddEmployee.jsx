95c69863f7acd5897ee13f2c5bd3653406f9a8c4
DenisGUCJ <denis.gorbatenk.main@gmail.com>
2019-11-25 22:03:00 +0100

    Lab7Ex2

--- src/AddEmployee.jsx
+++ src/AddEmployee.jsx
@@ -4,33 +4,45 @@ class AddEmployee extends React.Component{
 
     constructor(props){
         super(props);
+        this.state={
+            age:0
+        };
+        this.ageChangedEvent=this.ageChangedEvent.bind(this);
+    }
+    ageChangedEvent(event){
+        this.setState({
+            age : event.target.value
+        })
     }
 
     render(){
         return <div>
-        <form style={{border:'1px solid', padding : '10px',
-        width: 400,backgroundColor:"#ffe6e6"}} onSubmit={this.props.submit}>
-            <h2>Full fill the form and press "Submit" button to add a new employee to the data set</h2>
-            <p> IsActive:<br/>
-                <input name="isActive"/>
-            </p>
-            <p> Age:<br/>
-                <input name="age" type="number" min="0"/>
-            </p>
-            <p> Name:<br/>
-                <input name="name"/>
-            </p>
-            <p> Company:<br/>
-                <input name="company"/>
-            </p>
-            <p> Email:<br/>
-                <input name="email"/>
-            </p>
-            <p><input style={{width:100}} type="submit"/></p>
-            <p><input style={{width:100}} type="reset"/></p>
-            <p><button onClick={this.props.cancel} style={{width:100, textAlign:'center'}}>Cancel</button></p>
-        </form>
-    </div>
+            <form style={{border:'1px solid', padding : '10px',
+            width: 400,backgroundColor:"#ffe6e6"}} onSubmit={this.props.submit}>
+                <p> Age:<br/>
+                    <input name="age" type="number" onChange={this.ageChangedEvent} min="0"/>
+                </p>
+                {this.state.age>=18 ? <div><p> Name:<br/>
+                        <input name="name"/>
+                    </p>
+                    <p> Email:<br/>
+                        <input name="email"/>
+                    </p>
+                </div>
+                : 
+                <div><p> Parent Name:<br/>
+                        <input name="name"/>
+                    </p>
+                    <p>Parent Phone No:<br/>
+                        <input name="phone"/>
+                    </p>
+                </div>
+                }
+                <p><input style={{width:100}} type="submit"/></p>
+                <p><input style={{width:100}} type="reset"/></p>
+                <p><button onClick={this.props.cancel} style={{width:100, textAlign:'center'}}>Cancel</button></p>
+            </form>
+        </div>
     }
 }
 
