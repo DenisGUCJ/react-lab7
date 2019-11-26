3c997be9f7012a75e76721a9a29e4c142dca2b4a
DenisGUCJ <denis.gorbatenk.main@gmail.com>
2019-11-26 21:25:18 +0100

    Lab7Ex4

--- src/AddEmployee.jsx
+++ src/AddEmployee.jsx
@@ -6,8 +6,7 @@ class AddEmployee extends React.Component{
         super(props);
         this.state={
             age:0,
-            validEmail:true,
-            validPhone:true
+            validForm:false
         };
         this.ageChangedEvent=this.ageChangedEvent.bind(this);
         this.phoneChangedEvent=this.phoneChangedEvent.bind(this);
@@ -21,92 +20,63 @@ class AddEmployee extends React.Component{
     }
 
     phoneChangedEvent(event){
-        
-        const data = new FormData(event.target);
 
-        if(data.get("phone").match(/^\d{9}$/)){
+        if(event.target.value.match(/^\d{9}$/)){
             this.setState({
-                validPhone:true
+                validForm:true
             })
-            event.preventDefault();
         }
         else{
             this.setState({
-                validPhone:false
+                validForm:false
             })
-            event.preventDefault();
         }
     }
 
     emailChangedEvent(event){
 
-        const data = new FormData(event.target);
-
-        if(data.get("email").match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)){
+        if(event.target.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)){
             this.setState({
-                validEmail:true
+                validForm:true
             })
-            event.preventDefault();
         }
         else{
             this.setState({
-                validEmail:false
+                validForm:false
             })
-            event.preventDefault();
         }
     }
 
     render(){
         return <div>
             <form style={{border:'1px solid', padding : '10px',
-            width: 400,backgroundColor:"#d9e3f0"}} onSubmit={this.state.age<18 ?
-                this.phoneChangedEvent
-                :
-                this.emailChangedEvent
-                }>
+            width: 400,backgroundColor:"#d9e3f0"}}>
                 <p> Age:<br/>
-                    <input name="age" type="number" onChange={this.ageChangedEvent} min="0"/>
+                    <input name="age" type="number" onChange={this.ageChangedEvent} min="0" defaultValue="0"/>
                 </p>
-                {this.state.age>=18 ? <div><p> Name:<br/>
+                {this.state.age>=18 ? 
+                <div><p> Name:<br/>
                         <input name="name"/>
                     </p>
-                    {this.state.validEmail?
                     <div>
                         <p> Email:<br/>
-                            <input name="email"/>
+                            <input name="email" onChange={this.emailChangedEvent}/>
                         </p>
                     </div>
-                    :
-                    <div>
-                        <p> Email:<br/>
-                            <input style={{ border:'3px solid',borderColor:"#df6066"}} name="email"/>
-                        </p>
-                        <p>ERROR: the email is invalid</p>
-                    </div>
-                    }
-                    <p><input style={{width:100}} type="submit"/></p>
                 </div>
                 : 
-                <div><p> Parent Name:<br/>
+                <div>
+                    <p> Parent Name:<br/>
                         <input name="name"/>
                     </p>
-                    {this.state.validPhone ?
                     <div>
                         <p>Parent  No:<br/>
-                            <input name="phone" type="number"/>
+                            <input name="phone" type="number" onChange={this.phoneChangedEvent}/>
                         </p>
                     </div>
-                    :
-                    <div>
-                        <p>Parent No:<br/>
-                            <input style={{ border:'3px solid',borderColor:"#df6066"}} name="phone"/>
-                        </p>
-                        <p>ERROR: the phone is invalid</p>
-                    </div>
-                    }
-                    <p><input style={{width:100}} type="submit"/></p>
                 </div>
                 }
+                <p><input style={{width:100}} disabled={this.state.validForm ? "": "disabled"} type="submit"/></p>
                 <p><input style={{width:100}} type="reset"/></p>
                 <p><button onClick={this.props.cancel} style={{width:100, textAlign:'center'}}>Cancel</button></p>
             </form>
